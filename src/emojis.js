7e7514879a75fde8d999a6ae59ee9987317de3c1
ArkaRey <157515583+ArkaRey@users.noreply.github.com>
2024-04-22 13:40:34 +0200

    Add files via upload

---
+++ src/emojis.js
@@ -0,0 +1,10 @@
+module.exports = {
+    no: '<:StarsSteveangry:1187753039931637891>',
+    yes: '<a:StarsStevefingerguns:1187753036022554707>',
+    bad: '<:StarsSteveshybackgroundless:1187753440454135911>',
+    hehe: '<a:StarsStevefingerguns:1187753036022554707>',
+    haha: '<a:StarsStevescaredanimated:1187753436033323138>',
+    love: '<:StarsStevehug:1187753224988545115>',
+    dance: '<a:StarsSteveroll:1187753221289168938>',
+    euuhh: '<:StarsSteveconfused:1187752908125638789>'
+}
